3da665e0c010d19e9d0eaa8166f2ee6725078f37
SmithTM90 <smithtm90@gmail.com>
2016-11-14 12:06:28 -0800

    API should be wired up

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
 WikiViewer = angular.module('WikiViewer', [])
 
-WikiViewer.controller()
+WikiViewer.controller('WikiCtrl', ['$scope', '$http', function($scope,$http) {
+
+	$scope.Search = function() {
+		$http({
+			method: 'JSONP',
+			url: 'https://en.wikipedia.org/w/api.php',
+			params: {
+				action: 'opensearch',
+				search: $scope.searchTerm,
+				limit: 20,
+				namespace: 0,
+				format: 'json',
+				callback: 'JSON_CALLBACK'
+			}
+		}).then(function sucess(response) {
+			$scope.res = response.data;
+			console.log('success ', response);
+		}, function error(response) {
+			console.log('error ', response);
+		})
+	}
+
+}])
